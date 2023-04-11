bb4c5daeaa253bbd5befbcf49d00ff603faa6539
Viktoriia033 <vtorbych3@gmail.com>
2023-04-11 13:54:26 +0300

    task1

--- src/App.js
+++ src/App.js
@@ -2,23 +2,26 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+    let classNameItem = 'card';
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className={classNameItem}>
+          <h2>Homer</h2>
+          <img src="https://upload.wikimedia.org/wikipedia/en/0/02/Homer_Simpson_2006.png" />
+      </div>
+        <div className={classNameItem}>
+            <h2>Marg</h2>
+            <img src="https://upload.wikimedia.org/wikipedia/en/0/02/Homer_Simpson_2006.png" />
+        </div>
+        <div className={classNameItem}>
+            <h2>Bart</h2>
+            <img src="https://upload.wikimedia.org/wikipedia/en/0/02/Homer_Simpson_2006.png" />
+        </div>
+        <div className={classNameItem}>
+            <h2>Lisa</h2>
+            <img src="https://upload.wikimedia.org/wikipedia/en/0/02/Homer_Simpson_2006.png" />
+        </div>
+          </div>
   );
 }
 
