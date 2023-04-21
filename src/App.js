8356f3d16ab1b3e8ad303349848856cd96f92f9a
Viktoriia033 <vtorbych3@gmail.com>
2023-04-21 19:44:56 +0300

    task4_

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import {Link, Route, Routes} from "react-router-dom";
+import Todos from "./components-hw4/todos/todos";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+  <div>
+    <h2>Menu</h2>
+    <ul>
+      <li>
+        <Link to={'/'}>Todos</Link>
+      </li>
+    </ul>
+    <div>
+      <Routes>
+        <Route path={'/'} element={<Todos/>}/>
+      </Routes>
+    </div>
+  </div>
     </div>
   );
 }
---
+++ src/components-hw4/todoses/todoses.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Todoses = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Todoses;
